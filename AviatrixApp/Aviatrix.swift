34abfe2c89fa285bbb7e205c547be334114513c2
Apple <apple@Apples-MBP-5.attlocal.net>
2019-07-10 19:17:51 -0400

    complete interation 2 in progress

--- AviatrixApp/Aviatrix.swift
+++ AviatrixApp/Aviatrix.swift
@@ -13,6 +13,14 @@ class Aviatrix {
     var author = ""
     var data = AviatrixData()
     var currentLocation = "St. Louis"
+    var distanceTraveled = 0//miles
+    var maxFuel = 5000 //gallons
+    var fuelLevel = 5000.0//gallons
+    var milesPerGallon = 0.4//m/g
+    var fuelSpent = ""
+    var fuelCost = 0.0 //$
+    
+    
     
     init(username : String){
         author = username
@@ -27,12 +35,29 @@ class Aviatrix {
     }
     
     func refuel() {
+        let gallonsNeeded = Double(maxFuel)-fuelLevel
+        let data = AviatrixData()
+        fuelCost += gallonsNeeded * data.fuelPrices[current.Location]!
+        fuelLevel = 5000.0
+        return gallonsNeeded
         
     }
     
-    func flyTo(destination : String) {
+    func flyTo(destination : String){
+        distanceTraveled += distanceTo(currentLocation : currentLocation, target : destination)
         currentLocation = destination
+        
+       //create a full spent variable that calculates how much fuel you have used...
     }
+    
+    let fuelSpent = Double (distanceTraveled) /milesPerGallon!
+   
+    
+    
+    fuelLevel -= fuelSpent
+    currentLocation = destination
+    
+    //update fuel based on fuelSpent
     
     func distanceTo(currentLocation : String, target : String) -> Int {
         return data.knownDistances[currentLocation]![target]!
